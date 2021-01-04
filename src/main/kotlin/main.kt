a90226a78298dc1afc223402a8dd5602e317be7b
AdrienTurmo <adrien.turmo@gmail.com>
2021-01-04 14:52:23 +0100

    Roman Numerals: Use main to use it

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
+import kata.romanNumerals.ArabToRoman
+
+private val arabToRoman = ArabToRoman();
+
 fun main(args: Array<String>) {
-    println("Hello World! $args")
+    println("Katas :")
+    println("Convertion chiffre arabe en chiffre romain : 1")
+    print("Numéro du kata choisi : ")
+    val kata = readLine()!!
+    when (kata) {
+        "1" -> {
+            println("Convertir en chiffre romain")
+            print("Nombre à convertir : ")
+            val stringInput = readLine()!!
+
+            try {
+                val number = Integer.parseInt(stringInput)
+                println("Resultat : ${arabToRoman.convert(number)}")
+            } catch (e: NumberFormatException) {
+                println("Merci de renseigner un nombre compris entre 0 et 5000")
+            }
+        }
+        else -> println("Kata inconnu")
+    }
+
 }
