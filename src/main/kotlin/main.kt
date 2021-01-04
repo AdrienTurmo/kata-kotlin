7babf4a2edf4ad119789a5f830ee2e6b8b429706
AdrienTurmo <adrien.turmo@gmail.com>
2021-01-04 15:53:52 +0100

    Bonus main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,15 +2,19 @@ import kata.KataConvertArabToRoman
 import kata.KataConvertRomanToArab
 
 fun main(args: Array<String>) {
-    println("Katas :")
-    println("Convertion chiffre arabe en chiffre romain : 1")
-    println("Convertion chiffre romain en chiffre arabe : 2")
-    print("Numéro du kata choisi : ")
-    val kata = readLine()!!
-    when (kata) {
-        "1" -> KataConvertArabToRoman().execute()
-        "2" -> KataConvertRomanToArab().execute()
-        else -> println("Kata inconnu")
-    }
+    do {
+        println("Katas :")
+        println("Convertion chiffre arabe en chiffre romain : 1")
+        println("Convertion chiffre romain en chiffre arabe : 2")
+        println("Arreter le programme : 0")
+        print("Numéro du kata choisi : ")
+        val kata = readLine()!!
+        when (kata) {
+            "0" -> break;
+            "1" -> KataConvertArabToRoman().execute()
+            "2" -> KataConvertRomanToArab().execute()
+            else -> println("Kata inconnu")
+        }
+    } while (kata != "0")
 
 }
